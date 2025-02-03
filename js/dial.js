f7a9aca41be3a933c68e9b1cf9b8c006ccfbcaf8
y-r-s <thisissongyouran@gmail.com>
2025-02-03 14:11:26 +0000

    styled dial

--- js/dial.js
+++ js/dial.js
@@ -5,6 +5,7 @@ class RotatingDial {
     this.isDragging = false;
     this.startAngle = 0;
     this.currentAngle = 0;
+    this.lastValidAngle = 0; // Keep track of last valid angle
 
     this.init();
   }
@@ -16,6 +17,13 @@ class RotatingDial {
 
     this.dial = document.createElement("div");
     this.dial.className = "dial-circle";
+
+    // Add the indicator dot with number
+    const indicator = document.createElement("div");
+    indicator.className = "dial-indicator";
+    this.indicatorText = document.createTextNode("0");
+    indicator.appendChild(this.indicatorText);
+    this.dial.appendChild(indicator);
 
     this.container.appendChild(this.background);
     this.container.appendChild(this.dial);
@@ -43,23 +51,56 @@ class RotatingDial {
       clientY = event.clientY;
     }
 
-    const angle =
+    let angle =
       (Math.atan2(clientY - centerY, clientX - centerX) * 180) / Math.PI;
+    // Convert angle to 0-360 range
+    angle = (angle + 360) % 360;
     return angle;
+  }
+
+  normalizeAngle(angle) {
+    // Convert the angle to a 0-360 range
+    angle = (angle + 360) % 360;
+
+    // If the angle is in the allowed range (0-180), return it
+    if (angle <= 180) {
+      return angle;
+    }
+
+    // If the angle is closer to 0, return 0
+    if (angle > 270) {
+      return 0;
+    }
+    // If the angle is closer to 180, return 180
+    return 180;
   }
 
   startDragging(event) {
     event.preventDefault();
     this.isDragging = true;
-    this.startAngle = this.getAngle(event) - this.currentAngle;
+    const currentAngle = this.getAngle(event);
+    this.startAngle = currentAngle - this.lastValidAngle;
+  }
+
+  updateIndicatorNumber(angle) {
+    // Convert angle to number (0-5 range)
+    const number = Math.round((angle / 180) * 5);
+    this.indicatorText.nodeValue = number.toString();
   }
 
   drag(event) {
     if (!this.isDragging) return;
     event.preventDefault();
 
-    this.currentAngle = this.getAngle(event) - this.startAngle;
-    this.dial.style.transform = `translate(-50%, -50%) rotate(${this.currentAngle}deg)`;
+    let newAngle = this.getAngle(event) - this.startAngle;
+    newAngle = this.normalizeAngle(newAngle);
+
+    this.lastValidAngle = newAngle;
+    this.dial.style.transform = `translate(-50%, -50%) rotate(${newAngle}deg)`;
+    // Counter-rotate the indicator to keep text upright
+    const indicator = this.dial.querySelector(".dial-indicator");
+    indicator.style.transform = `translate(-50%, -50%) rotate(${-newAngle}deg)`;
+    this.updateIndicatorNumber(newAngle);
   }
 
   stopDragging() {
